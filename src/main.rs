b9140bd64967512d3ee5b43177e030d6a3b9b221
Cory Benfield <lukasaoz@gmail.com>
2015-08-21 10:30:10 +0000

    Channels for receiving ARPs

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,18 @@ use libc::{c_int, c_short, c_void, AF_PACKET};
 use libc::{socket, recv, close};
 use errno::errno;
 
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::thread;
 
 static SOCK_RAW: c_int = 3;
 static ETH_P_ARP: c_short = 0x0806;
 
-fn listen_for_arps() {
+struct GARP {
+    ip: [u8; 4],
+    mac: [u8; 6],
+}
+
+fn listen_for_arps(arp_tx: Sender<GARP>) {
     let sock = unsafe { socket(AF_PACKET, SOCK_RAW, ETH_P_ARP.to_be() as c_int) };
     if sock < 0 {
         error!("Failed to open socket: {}, {}", sock, errno());
@@ -52,14 +58,24 @@ fn listen_for_arps() {
             continue;
         }
 
-        println!("Gratuitous ARP! IP: {}.{}.{}.{}, MAC: {:x}:{:x}:{:x}:{:x}:{:x}:{:x}", spa[0], spa[1], spa[2], spa[3], sha[0], sha[1], sha[2], sha[3], sha[4], sha[5]);
+        let garp = GARP {
+            ip: [spa[0], spa[1], spa[2], spa[3]],
+            mac: [sha[0], sha[1], sha[2], sha[3], sha[4], sha[5]],
+        };
+
+        arp_tx.send(garp).unwrap(); // Handle error.
+
     }
 
     unsafe { close(sock) };
 }
 
-fn socket_handler() {
-    loop {}
+fn socket_handler(arp_rx: Receiver<GARP>) {
+    loop {
+        let arp = arp_rx.recv().unwrap();  // Handle error.
+
+        println!("Gratuitous ARP! IP: {}.{}.{}.{}, MAC: {:x}:{:x}:{:x}:{:x}:{:x}:{:x}", arp.ip[0], arp.ip[1], arp.ip[2], arp.ip[3], arp.mac[0], arp.mac[1], arp.mac[2], arp.mac[3], arp.mac[4], arp.mac[5]);
+    }
 }
 
 fn main() {
@@ -67,8 +83,9 @@ fn main() {
     info!("Launching garpd");
 
     // Spawn work threads.
-    let x = thread::spawn(move || listen_for_arps());
-    let y = thread::spawn(move || socket_handler());
+    let (arp_tx, arp_rx) = channel();
+    let x = thread::spawn(move || listen_for_arps(arp_tx));
+    let y = thread::spawn(move || socket_handler(arp_rx));
     x.join().unwrap();
     y.join().unwrap();
 }
