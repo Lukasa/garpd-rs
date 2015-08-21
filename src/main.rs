5a03b8fd756173c54db91438c57aca60b48d0e23
Cory Benfield <lukasaoz@gmail.com>
2015-08-21 10:01:57 +0000

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-extern crate libc;
 extern crate errno;
+extern crate libc;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 use libc::{c_int, c_short, c_void, AF_PACKET};
 use libc::{socket, recv, close};
 use errno::errno;
@@ -8,9 +12,12 @@ static SOCK_RAW: c_int = 3;
 static ETH_P_ARP: c_short = 0x0806;
 
 fn main() {
+    env_logger::init().unwrap();
+
+    info!("Launching garpd");
     let sock = unsafe { socket(AF_PACKET, SOCK_RAW, ETH_P_ARP.to_be() as c_int) };
     if sock < 0 {
-        println!("Failed to open socket: {}, {}", sock, errno());
+        error!("Failed to open socket: {}, {}", sock, errno());
         return
     }
 
@@ -18,22 +25,22 @@ fn main() {
         let mut buf: [u8; 65535] = [0; 65535];
         let rc = unsafe { recv(sock, buf.as_mut_ptr() as *mut c_void, buf.len() as u64, 0) };
         if rc == 0 {
-            println!("Zero length read!");
+            error!("Zero length socket read!");
             break;
         }
 
         if rc < 14 {
-            println!("Ethernet frame too short!");
+            info!("Ethernet frame too short!");
             continue;
         }
 
         if ((buf[12] as i16) << 8) + (buf[13] as i16) != ETH_P_ARP {
-            println!("Not arp packet");
+            info!("Not arp packet");
             continue;
         }
 
         if rc < 43 {
-            println!("ARP packet too short!");
+            info!("ARP packet too short!");
             continue;
         }
 
